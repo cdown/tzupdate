47977f262a9501f95e9de84b39acb11544b5adb8
Chris Down <chris@chrisdown.name>
2013-08-23 17:09:22 +0200

    Update setup.py to 0.3.0.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(
     name = "tzupdate",
-    version = "0.2.1",
-    description = "Set the local timezone based on IP geolocation.",
+    version = "0.3.0",
+    description = "Set the system timezone based on IP geolocation.",
     author = "Chris Down",
     author_email = "chris@chrisdown.name",
-    url = "http://chrisdown.name",
+    url = "https://github.com/cdown/tzupdate",
     scripts = [ "tzupdate" ],
 )
