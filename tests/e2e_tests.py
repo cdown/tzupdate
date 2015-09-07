bfad57d82826b6905e01bdd6f82743b9b76dd294
Chris Down <chris@chrisdown.name>
2015-09-07 09:51:14 +0100

    Add e2e test for explicit IP passthrough

--- tests/e2e_tests.py
+++ tests/e2e_tests.py
@@ -8,8 +8,7 @@ from nose.tools import assert_raises, eq_ as eq, assert_true, assert_false
 
 @mock.patch('tzupdate.get_timezone_for_ip')
 @mock.patch('tzupdate.link_localtime')
-def test_end_to_end_default_success(link_localtime_mock,
-                                    get_timezone_for_ip_mock):
+def test_end_to_end_no_args(link_localtime_mock, get_timezone_for_ip_mock):
     get_timezone_for_ip_mock.return_value = 'Fake/Timezone'
     tzupdate.run([])
     assert_true(link_localtime_mock.called)
@@ -17,8 +16,7 @@ def test_end_to_end_default_success(link_localtime_mock,
 
 @mock.patch('tzupdate.get_timezone_for_ip')
 @mock.patch('tzupdate.link_localtime')
-def test_print_only_should_not_link(link_localtime_mock,
-                                    get_timezone_for_ip_mock):
+def test_print_only_no_link(link_localtime_mock, get_timezone_for_ip_mock):
     get_timezone_for_ip_mock.return_value = 'Fake/Timezone'
     tzupdate.run(['-p'])
     assert_false(link_localtime_mock.called)
@@ -26,10 +24,17 @@ def test_print_only_should_not_link(link_localtime_mock,
 
 @mock.patch('tzupdate.get_timezone_for_ip')
 @mock.patch('tzupdate.link_localtime')
-def test_explicit_paths(link_localtime_mock,
-                                get_timezone_for_ip_mock):
+def test_explicit_paths(link_localtime_mock, get_timezone_for_ip_mock):
     get_timezone_for_ip_mock.return_value = 'Fake/Timezone'
     tzupdate.run(['-l', '/l', '-z', '/z'])
     assert_true(
         link_localtime_mock.called_once_with('Fake/Timezone', '/z', '/l'),
     )
+
+
+@mock.patch('tzupdate.get_timezone_for_ip')
+@mock.patch('tzupdate.link_localtime')
+def test_explicit_ip(link_localtime_mock, get_timezone_for_ip_mock):
+    get_timezone_for_ip_mock.return_value = 'Fake/Timezone'
+    tzupdate.run(['-a', '1.2.3.4'])
+    assert_true(get_timezone_for_ip_mock.called_once_with('1.2.3.4'))
