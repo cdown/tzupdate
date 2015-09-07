695652b7c8b7671c66c437daf3b111b7e7d4fbf3
Chris Down <chris@chrisdown.name>
2015-09-07 09:44:48 +0100

    Add test to check for explicit path passthrough

--- tests/e2e_tests.py
+++ tests/e2e_tests.py
@@ -22,3 +22,14 @@ def test_print_only_should_not_link(link_localtime_mock,
     get_timezone_for_ip_mock.return_value = 'Fake/Timezone'
     tzupdate.run(['-p'])
     assert_false(link_localtime_mock.called)
+
+
+@mock.patch('tzupdate.get_timezone_for_ip')
+@mock.patch('tzupdate.link_localtime')
+def test_explicit_paths(link_localtime_mock,
+                                get_timezone_for_ip_mock):
+    get_timezone_for_ip_mock.return_value = 'Fake/Timezone'
+    tzupdate.run(['-l', '/l', '-z', '/z'])
+    assert_true(
+        link_localtime_mock.called_once_with('Fake/Timezone', '/z', '/l'),
+    )
