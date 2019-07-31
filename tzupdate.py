330fde4c5c2dc496c57d21d4a465fe2f30a8fd08
Chris Down <chris@chrisdown.name>
2019-07-31 16:43:02 +0100

    services: Remove timezoneapi.io, they started requiring tokens

--- tzupdate.py
+++ tzupdate.py
@@ -39,11 +39,6 @@ SERVICES = frozenset(
         GeoIPService("https://freegeoip.app/json/{ip}", ("time_zone",), None),
         GeoIPService("https://ipapi.co/{ip}/json/", ("timezone",), ("reason",)),
         GeoIPService("http://worldtimeapi.org/api/ip/{ip}", ("timezone",), ("error",)),
-        GeoIPService(
-            "https://timezoneapi.io/api/ip/?{ip}",
-            ("data", "timezone", "id"),
-            ("meta", "message"),
-        ),
     ]
 )
 
